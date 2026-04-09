6962822650f20940edc31dda069f3829333e7e4e
Fabrice-Code <fabricecelik@gmail.com>
2026-04-09 21:15:23 +0200

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> d48c480b76e43640fcf3309d5a6028869f61b1df
 })
